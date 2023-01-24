dd3b663cd9f25bee04239a3edd1ad5f1bb09ae16
auxten <auxtenwpc@gmail.com>
2023-01-24 15:16:35 +0000

    Add chdb python project

---
+++ chdb/tests/conftest.py
@@ -0,0 +1,10 @@
+"""
+    Dummy conftest.py for chdb.
+
+    If you don't know what this is for, just leave it empty.
+    Read more about conftest.py under:
+    - https://docs.pytest.org/en/stable/fixture.html
+    - https://docs.pytest.org/en/stable/writing_plugins.html
+"""
+
+# import pytest
