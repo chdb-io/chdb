db46749f2c8525397cadf195b28d0d1a3ca5a9a8
auxten <auxtenwpc@gmail.com>
2023-01-26 11:18:00 +0000

    Add args for test_core

--- pybind/test_core.py
+++ pybind/test_core.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 import concurrent.futures
 import time
-import chdb
+import sys
+import _chdb as chdb
 
 # run query parallel in n thread and benchmark
-thread_count = 10
-query_count = 10000
+thread_count = 1
+query_count = 1000
+
+if len(sys.argv) == 2:
+    thread_count = int(sys.argv[1])
+elif len(sys.argv) == 3:
+    thread_count = int(sys.argv[1])
+    query_count = int(sys.argv[2])
+
 thread_pool = concurrent.futures.ThreadPoolExecutor(max_workers=thread_count)
 
 def run_query(query, format):
-    res = chdb.queryToBytes(query, format)
-    print(len(res.tobytes()))
-    res.release()
+    res = chdb.query(query, format)
+    print(len(res.get_memview().tobytes()))
 
 def run_queries(query, format, count = query_count):
     for i in range(count):
@@ -33,4 +40,3 @@ def benchmark(query, format, parallel = thread_count, count = query_count):
 
 if __name__ == '__main__':
     benchmark("SELECT * FROM file('/home/Clickhouse/bench/result.parquet', Parquet) LIMIT 10", "Arrow")
-    
