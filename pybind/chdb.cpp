fa2a7844e4c813daffc86e7cc6aa3879b474cd4b
auxten <auxtenwpc@gmail.com>
2023-01-26 11:16:56 +0000

    Add class query_result

--- pybind/chdb.cpp
+++ pybind/chdb.cpp
@@ -1,24 +1,47 @@
 #include "chdb.h"
 
 #include <iostream>
-// #include <string_view>
-// #include <arrow/api.h>
-// #include <arrow/buffer.h>
-// #include <arrow/io/memory.h>
-// #include <arrow/ipc/api.h>
-// #include <arrow/python/pyarrow.h>
-#include <pybind11/numpy.h>
-#include <pybind11/pybind11.h>
-#include <pybind11/pytypes.h>
-#include <pybind11/stl.h>
+#include "pybind11/numpy.h"
+#include "pybind11/pybind11.h"
+#include "pybind11/pytypes.h"
+#include "pybind11/stl.h"
 
 namespace py = pybind11;
 
 bool inside_main = true;
 
+class query_result
+{
+private:
+    local_result * result;
+    py::memoryview * memview;
+
+public:
+    query_result(local_result * result) : result(result)
+    {
+        if (result)
+        {
+            memview = new py::memoryview(py::memoryview::from_memory(result->buf, result->len, true));
+        }
+        else
+        {
+            memview = new py::memoryview(py::memoryview::from_memory(nullptr, 0, true));
+        }
+    }
+    ~query_result()
+    {
+        free_result(result);
+        delete memview;
+    }
+
+    char * data() { return result->buf; }
+    size_t size() { return result->len; }
+    py::memoryview get_memview() { return *memview; }
+};
+
 local_result * queryToBuffer(const std::string & queryStr, const std::string & format = "CSV")
 {
-    char* argv_[] = {(char*)"clickhouse", (char*)"--multiquery"};
+    char * argv_[] = {(char *)"clickhouse", (char *)"--multiquery"};
 
     std::vector<char *> argv(argv_, argv_ + sizeof(argv_) / sizeof(argv_[0]));
     std::unique_ptr<char[]> formatStr(new char[format.size() + 17]);
@@ -30,14 +53,37 @@ local_result * queryToBuffer(const std::string & queryStr, const std::string & f
     return query_stable(argv.size(), argv.data());
 }
 
-std::shared_ptr<std::vector<char>> queryToVector(const std::string & queryStr, const std::string & format = "CSV")
+std::unique_ptr<query_result> query(const std::string & queryStr, const std::string & format = "CSV")
 {
-    auto result = queryToBuffer(queryStr, format);
+    return std::make_unique<query_result>(queryToBuffer(queryStr, format));
+}
+
+#ifdef PY_TEST_MAIN
+#    include <string_view>
+#    include <arrow/api.h>
+#    include <arrow/buffer.h>
+#    include <arrow/io/memory.h>
+#    include <arrow/ipc/api.h>
+#    include <arrow/python/pyarrow.h>
+
+
+std::shared_ptr<arrow::Table> queryToArrow(const std::string & queryStr)
+{
+    auto result = queryToBuffer(queryStr, "Arrow");
     if (result)
     {
-        auto vec = std::make_shared<std::vector<char>>(result->buf, result->buf + result->len);
+        // Create an Arrow input stream from the Arrow buffer
+        auto input_stream = std::make_shared<arrow::io::BufferReader>(reinterpret_cast<uint8_t *>(result->buf), result->len);
+        auto arrow_reader = arrow::ipc::RecordBatchFileReader::Open(input_stream, result->len).ValueOrDie();
+
+        // Read all the record batches from the Arrow reader
+        auto batch = arrow_reader->ReadRecordBatch(0).ValueOrDie();
+        std::shared_ptr<arrow::Table> arrow_table = arrow::Table::FromRecordBatches({batch}).ValueOrDie();
+
+        // Free the memory used by the result
         free_result(result);
-        return vec;
+
+        return arrow_table;
     }
     else
     {
@@ -45,56 +91,6 @@ std::shared_ptr<std::vector<char>> queryToVector(const std::string & queryStr, c
     }
 }
 
-//same as queryToVector but return a CSV string
-std::string queryToCSV(const std::string & queryStr)
-{
-    auto vec = queryToVector(queryStr);
-    if (vec)
-    {
-        // std::cerr << "vec->size() = " << vec->size() << std::endl;
-        return std::string(vec->begin(), vec->end());
-    }
-    else
-    {
-        return {};
-    }
-}
-
-// std::shared_ptr<arrow::Table> queryToArrow(const std::string & queryStr)
-// {
-//     auto result = queryToBuffer(queryStr, "Arrow");
-//     if (result)
-//     {
-//         // Create an Arrow input stream from the Arrow buffer
-//         auto input_stream = std::make_shared<arrow::io::BufferReader>(reinterpret_cast<uint8_t *>(result->buf), result->len);
-//         auto arrow_reader = arrow::ipc::RecordBatchFileReader::Open(input_stream, result->len).ValueOrDie();
-
-//         // Read all the record batches from the Arrow reader
-//         auto batch = arrow_reader->ReadRecordBatch(0).ValueOrDie();
-//         std::shared_ptr<arrow::Table> arrow_table = arrow::Table::FromRecordBatches({batch}).ValueOrDie();
-
-//         // Free the memory used by the result
-//         free_result(result);
-
-//         return arrow_table;
-//     }
-//     else
-//     {
-//         return nullptr;
-//     }
-// }
-
-// py::object queryToArrowObject(const std::string & queryStr)
-// {
-//     std::shared_ptr<arrow::Table> table = queryToArrow(queryStr);
-//     // Wrap the table in a pyarrow.Table object
-//     auto py_table = arrow::py::wrap_table(table);
-
-//     // Use py::reinterpret_borrow to convert PyObject* to py::object
-//     return py::reinterpret_borrow<py::object>(py_table);
-// }
-
-#ifdef PY_TEST_MAIN
 int main()
 {
     // auto out = queryToVector("SELECT * FROM file('/home/Clickhouse/bench/result.parquet', Parquet) LIMIT 10");
@@ -114,64 +110,13 @@ PYBIND11_MODULE(_chdb, m)
 {
     m.doc() = "My module for query function";
 
-    // Bind the query function to the module
-    // m.def(
-    //     "query",
-    //     &queryToVector,
-    //     pybind11::arg("queryStr"),
-    //     pybind11::return_value_policy::reference_internal,
-    //     "Execute a query and return a vector of chars");
+    py::class_<query_result>(m, "query_result")
+        .def(py::init<local_result *>())
+        .def("data", &query_result::data)
+        .def("size", &query_result::size)
+        .def("get_memview", &query_result::get_memview);
 
-    // m.def("queryToVector", &queryToVector, "Execute SQL query");
-    m.def("queryToCSV", &queryToCSV, "Execute SQL query and return CSV");
-    m.def(
-        "queryToBytes",
-        [](const std::string & queryStr, const std::string & format) -> py::memoryview
-        {
-            auto result = queryToBuffer(queryStr, format);
-            if (!result)
-            {
-                return py::memoryview::from_memory(nullptr, 0, true);
-            }
-            py::memoryview memview = py::memoryview::from_memory(result->buf, result->len);
-            // free_result(result);
-            return memview;
-        },
-        "Execute SQL query and return bytes");
-    // m.def("queryToArrow", [](const std::string & queryStr) -> std::string
-    // {
-    //     auto table = queryToArrow(queryStr);
-    //     if (table)
-    //     {
-    //         // // Create a Python Capsule object
-    //         // py::capsule arrow_table_capsule(
-    //         //     table.get(),
-    //         //     [](void * ptr)
-    //         //     {
-    //         //         // Free the shared_ptr to the arrow::Table
-    //         //         arrow::Table * table = reinterpret_cast<arrow::Table *>(ptr);
-    //         //         delete table;
-    //         //     });
-    //         // Create a pyarrow.lib.Table object from the arrow::Table Capsule
-    //         py::module pyarrow = py::module::import("pyarrow");
-    //         auto arrow_table = pyarrow.attr("lib").attr("Table").attr("_import_from_c")("_chdb", "arrow_table");
-    //         // Convert the pyarrow.lib.Table object to a pyarrow.Buffer object
-    //         auto arrow_buffer = arrow_table.attr("to_batches")().attr("serialize")();
-    //         // Convert the pyarrow.Buffer object to a Python bytes object
-    //         auto py_bytes = arrow_buffer.attr("to_pybytes")();
-    //         // Convert the Python bytes object to a std::string
-    //         return py_bytes.cast<std::string>();
-    //     }
-    //     else
-    //     {
-    //         return {};
-    //     }
-    // }
-    // , "Execute SQL query and return Arrow Table");
-    // m.def(
-    //     "queryToArrowObject",
-    //     &queryToArrowObject,
-    //     py::return_value_policy::take_ownership);
+    m.def("query", &query, "A function which queries Clickhouse and returns a query_result object");
 }
 
 #endif // PY_TEST_MAIN
