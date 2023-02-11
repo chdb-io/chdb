dbd8914e035c8f8531a0b80e5924d5edbc765692
auxten <auxtenwpc@gmail.com>
2023-02-11 18:53:35 +0800

    Fix macos build by combine libchdb.so and _chdb

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import sys
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 import setuptools
+from distutils import log
+
+log.set_verbosity(log.DEBUG)
 
 # get the path of the current file
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -11,10 +14,19 @@ libdir = os.path.join(script_dir, "chdb")
 # Determine which compiler to use, prefer clang++ over g++
 if os.system('which clang++ > /dev/null') == 0:
     print("Using Clang")
-    os.environ['CC'] = 'clang++'
+    # if macOS, prefer brew clang++ compiler over the system one
+    if sys.platform == 'darwin':
+        if os.system('which /usr/local/opt/llvm/bin/clang++ > /dev/null') == 0:
+            # distutils likes to use CC as cpp, so set CC to clang++
+            os.environ['CC'] = '/usr/local/opt/llvm/bin/clang++'
+            os.environ['CXX'] = '/usr/local/opt/llvm/bin/clang++'
+    else:
+        os.environ['CC'] = 'clang++'
+        os.environ['CXX'] = 'clang++'
 else:
     print("Using GCC")
-    os.environ['CC'] = 'g++'
+    os.environ['CC'] = 'gcc'
+    os.environ['CXX'] = 'g++'
 
 # As of Python 3.6, CCompiler has a `has_flag` method.
 # cf http://bugs.python.org/issue26689
@@ -56,16 +68,18 @@ class BuildExt(build_ext):
     link_opts = {
         # we must use relative path here, otherwise the '_chdb' package will not be able to find the './libchdb.so' file
         # see the 'chdb/__init__.py' file for more details
-        'unix': ['-shared', '-Wl,--exclude-libs,ALL', '-static-libstdc++', '-static-libgcc', './libchdb.so'],
+        'unix': [],
         'msvc': [],
     }
 
     if sys.platform == 'darwin':
-        # c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-        c_opts['unix'] += ['-mmacosx-version-min=10.7']
-    # else:
-    #     c_opts['unix'].append("-fPIC")
-    #     link_opts['unix'].append('-stdlib=libstdc++')
+        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=12.0']
+        link_opts['unix'] += ['-static-libstdc++', '-stdlib=libc++', '-mmacosx-version-min=12.0']
+        link_opts['unix'] += ['-lchdb', '-Wl,-rpath,' + libdir]
+        # link_opts['unix'].append('-dynamiclib')
+    else:
+        link_opts['unix'] += ['-shared', '-Wl,--exclude-libs,ALL', '-static-libgcc', '-static-libstdc++', '-stdlib=libstdc++']
+        link_opts['unix'] += ['./libchdb.so']
 
     def build_extensions(self):
         ct = self.compiler.compiler_type
@@ -88,7 +102,37 @@ class BuildExt(build_ext):
                 # Path to pybind11 headers
                 pybind11.get_include(),
             ])
-        
+
+        if sys.platform == 'darwin':
+            if os.environ["CC"] == "/usr/local/opt/llvm/bin/clang++":
+                clean_compiler = []
+                for arg in self.compiler.compiler:
+                    if arg == "-I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include":
+                        clean_compiler.append("-I/usr/local/opt/llvm/include/c++/v1")
+                    else:
+                        clean_compiler.append(arg)
+                self.compiler.compiler = clean_compiler
+                self.compiler.compiler_so = clean_compiler
+                
+            clean_linker = []
+            for arg in self.compiler.linker_so:
+                if arg == "-bundle":
+                    clean_linker.append("-dynamiclib")
+                # elif arg == "-undefined":
+                #     continue
+                # elif arg == "dynamic_lookup":
+                #     continue
+                elif arg == "-L/usr/local/opt/readline/lib":
+                    continue
+                elif arg == "-L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib":
+                    if os.environ["CC"] == "/usr/local/opt/llvm/bin/clang++":
+                        clean_linker.append("-L/usr/local/opt/llvm/lib")
+                    else:
+                        clean_linker.append(arg)
+                else:
+                    clean_linker.append(arg)
+
+            self.compiler.linker_so = clean_linker
         # change the current working directory to the path of the current file
         # and compile and link the './libchdb.so' then change the working directory back
         cwd = os.getcwd()
@@ -103,17 +147,17 @@ if __name__ == "__main__":
         library_file = os.path.join(libdir, "libchdb.so")
         source_files = ['chdb.cpp']
         extra_objects = []
-        if os.path.exists(library_file):
+        if not os.path.exists(library_file):
             # if we have a prebuilt library file, use that.
-            extra_objects.append(library_file)
-        else:
+            # extra_objects.append(library_file)
+        # else:
             # otherwise, run build.sh to build the library file.
             # if this fails, the setup.py will fail.
             # os.system("bash chdb/build.sh")
             ret = os.system("bash chdb/build.sh")
             if ret != 0:
                 raise RuntimeError("Build failed")
-            extra_objects.append(library_file)
+            # extra_objects.append(library_file)
         
         ext_modules = [
             Extension(
@@ -121,7 +165,9 @@ if __name__ == "__main__":
                 source_files,
                 include_dirs=['../', '../base', '../src', '../programs/local'],
                 language='c++',
-                extra_objects=extra_objects,
+                libraries=['chdb'],
+                library_dirs=[libdir],
+                # extra_objects=extra_objects,
             ),
         ]
         
