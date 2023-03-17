17b8d8505b3e45bd1cb8ecc6c2c91ef635cdea9b
auxten <auxtenwpc@gmail.com>
2023-03-17 17:38:51 +0800

    Fix github CI
    diz
    
    diz
    
    diz
    
    diz
    
    diz
    
    diz
    
    diz
    
    diz
    
    diz
    
    diz

--- setup.py
+++ setup.py
@@ -49,22 +49,23 @@ class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
 
     def build_extensions(self):
-        # Determine which compiler to use, prefer clang++ over g++
-        if os.system('which clang++ > /dev/null') == 0:
-            print("Using Clang")
-            # if macOS, prefer brew clang++ compiler over the system one
-            if sys.platform == 'darwin':
-                if os.system('which /usr/local/opt/llvm/bin/clang++ > /dev/null') == 0:
-                    # distutils likes to use CC as cpp, so set CC to clang++
-                    os.environ['CC'] = '/usr/local/opt/llvm/bin/clang++'
-                    os.environ['CXX'] = '/usr/local/opt/llvm/bin/clang++'
+        # Determine which compiler to use, if CC and CXX env exist, use them
+        if os.environ.get('CC') is not None and os.environ.get('CXX') is not None:
+            print("Using CC and CXX from env")
+            print("CC: " + os.environ.get('CC'))
+            print("CXX: " + os.environ.get('CXX'))
+            return
+        if sys.platform == 'darwin':
+            if os.system('which /usr/local/opt/llvm/bin/clang++ > /dev/null') == 0:
+                os.environ['CC'] = '/usr/local/opt/llvm/bin/clang'
+                os.environ['CXX'] = '/usr/local/opt/llvm/bin/clang++'
             else:
-                os.environ['CC'] = 'clang++'
-                os.environ['CXX'] = 'clang++'
+                raise RuntimeError("Must use brew clang++")
+        elif sys.platform == 'linux':
+            os.environ['CC'] = 'clang-15'
+            os.environ['CXX'] = 'clang++-15'
         else:
-            print("Using GCC")
-            os.environ['CC'] = 'gcc'
-            os.environ['CXX'] = 'g++'
+            raise RuntimeError("Unsupported platform")
 
         ret = os.system("bash chdb/build.sh")
         if ret != 0:
