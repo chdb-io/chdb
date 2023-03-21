089a7531e41d95ffb60b778fcdd521981758a331
auxten <auxtenwpc@gmail.com>
2023-03-21 09:20:08 +0800

    Use python instead of python3-config

--- chdb/vars.sh
+++ chdb/vars.sh
@@ -4,7 +4,7 @@ PROJ_DIR="${DIR}/.." # project root directory
 BUILD_DIR="$PROJ_DIR/buildlib" # build directory
 CHDB_DIR="$PROJ_DIR/chdb" # chdb directory
 CHDB_PY_MOD="_chdb"
-CHDB_PY_MODULE="${CHDB_PY_MOD}$(python3-config --extension-suffix)" # python module name
+CHDB_PY_MODULE=${CHDB_PY_MOD}$(python3 -c "import sysconfig; print(sysconfig.get_config_var('EXT_SUFFIX'))")
 
 # check current os type, and make ldd command
 if [ "$(uname)" == "Darwin" ]; then
