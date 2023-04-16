0d38437c3da526876822d5e894ebc80d512ffc81
auxten <auxten@users.noreply.github.com>
2023-04-16 11:14:24 +0800

    Add to pandas example (#5)

---
+++ examples/to_pandas.py
@@ -0,0 +1,31 @@
+#!python3
+import os
+import pyarrow as pa
+import chdb
+
+# get current file dir
+current_dir = os.path.dirname(os.path.abspath(__file__))
+test_parquet = current_dir + "/../contrib/arrow/cpp/submodules/parquet-testing/data/alltypes_dictionary.parquet"
+
+# run SQL on parquet file and return arrow format
+res = chdb.query(f"select * from file('{test_parquet}', Parquet)", "Arrow")
+print("\nresult from chdb:")
+print(res.get_memview().tobytes())
+
+def to_arrowTable(res):
+    # convert arrow format to arrow table
+    paTable = pa.RecordBatchFileReader(res.get_memview()).read_all()
+    return paTable
+
+def to_df(res):
+    # convert arrow format to arrow table
+    paTable = to_arrowTable(res)
+    # convert arrow table to pandas dataframe
+    return paTable.to_pandas(use_threads=True)
+
+print("\nresult from chdb to pyarrow:")
+print(to_arrowTable(res))
+
+# convert arrow table to pandas dataframe
+print("\nresult from chdb to pandas:")
+print(to_df(res))
