fb75b18fbeacefad77b391db8d0284c007f5acc7
laodouya <xujin3@xunlei.com>
2023-05-18 15:09:29 +0800

    Add db api 2.0 driver

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ def fix_version_init(version):
         f.seek(0)
         f.write(init_content)
         f.truncate()
-    
+
 
 # As of Python 3.6, CCompiler has a `has_flag` method.
 # cf http://bugs.python.org/issue26689
@@ -117,12 +117,16 @@ class BuildExt(build_ext):
             print("CC: " + os.environ.get('CC'))
             print("CXX: " + os.environ.get('CXX'))
         if sys.platform == 'darwin':
-            if os.system('which /usr/local/opt/llvm/bin/clang++ > /dev/null') == 0:
-                os.environ['CC'] = '/usr/local/opt/llvm/bin/clang'
-                os.environ['CXX'] = '/usr/local/opt/llvm/bin/clang++'
-            elif os.system('which /usr/local/opt/llvm@15/bin/clang++ > /dev/null') == 0:
-                os.environ['CC'] = '/usr/local/opt/llvm@15/bin/clang'
-                os.environ['CXX'] = '/usr/local/opt/llvm@15/bin/clang++'
+            try:
+                brew_prefix = subprocess.check_output('brew --prefix', shell=True).decode("utf-8").strip("\n")
+            except Exception:
+                raise RuntimeError("Must install brew")
+            if os.system('which '+brew_prefix+'/opt/llvm/bin/clang++ > /dev/null') == 0:
+                os.environ['CC'] = brew_prefix + '/opt/llvm/bin/clang'
+                os.environ['CXX'] = brew_prefix + '/opt/llvm/bin/clang++'
+            elif os.system('which '+brew_prefix+'/opt/llvm@15/bin/clang++ > /dev/null') == 0:
+                os.environ['CC'] = brew_prefix + '/opt/llvm@15/bin/clang'
+                os.environ['CXX'] = brew_prefix + '/opt/llvm@15/bin/clang++'
             else:
                 raise RuntimeError("Must use brew clang++")
         elif sys.platform == 'linux':
