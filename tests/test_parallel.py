16964167d54ca46275cf31b15018d0679d93f60b
auxten <auxtenwpc@gmail.com>
2023-07-14 02:48:43 +0000

    Optimize test_parallel

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -2,9 +2,8 @@
 import concurrent.futures
 import time
 import sys
-import os
-import chdb
 import unittest
+import chdb
 from utils import data_file
 
 # run query parallel in n thread and benchmark
@@ -20,30 +19,30 @@ elif len(sys.argv) == 3:
 thread_pool = concurrent.futures.ThreadPoolExecutor(max_workers=thread_count)
 
 
-def run_query(query, format):
-    res = chdb.query(query, format)
+def run_query(query, fmt):
+    res = chdb.query(query, fmt)
     if len(res) < 2000:
         print(f"Error: result size is not correct {res.bytes()}")
         exit(1)
 
 
-def run_queries(query, format, count=query_count):
-    for i in range(count):
-        run_query(query, format)
+def run_queries(query, fmt, count=query_count):
+    for _ in range(count):
+        run_query(query, fmt)
 
 
-def run_queries_parallel(query, format, parallel=thread_count, count=query_count):
-    for i in range(parallel):
-        thread_pool.submit(run_queries, query, format, count // parallel)
+def run_queries_parallel(query, fmt, parallel=thread_count, count=query_count):
+    for _ in range(parallel):
+        thread_pool.submit(run_queries, query, fmt, count // parallel)
 
 
 def wait():
     thread_pool.shutdown(wait=True)
 
 
-def benchmark(query, format, parallel=thread_count, count=query_count):
+def benchmark(query, fmt, parallel=thread_count, count=query_count):
     time_start = time.time()
-    run_queries_parallel(query, format, parallel, count)
+    run_queries_parallel(query, fmt, parallel, count)
     wait()
     time_end = time.time()
     print("Time cost:", time_end - time_start, "s")
