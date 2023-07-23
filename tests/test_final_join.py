48cf7a9b85298f362d3d907cc3d44c0945b6678d
auxten <auxtenwpc@gmail.com>
2023-07-23 04:17:18 +0000

    Add session related tests

---
+++ tests/test_final_join.py
@@ -0,0 +1,23 @@
+#!python3
+
+import unittest
+import psutil
+from chdb import session
+
+
+current_process = psutil.Process()
+
+
+class TestStateful(unittest.TestCase):
+    def test_zfree_thread_count(self):
+        sess2 = session.Session()
+        ret = sess2.query("SELECT sleep(2)", "Debug")
+        # self.assertEqual(str(ret), "")
+        thread_count = current_process.num_threads()
+        print("Number of threads using psutil library: ", thread_count)
+        self.assertEqual(thread_count, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
