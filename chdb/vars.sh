ecd019493e8d89b892c046c4b4e2777d0a57b7a3
nmreadelf <7260482+nmreadelf@users.noreply.github.com>
2023-08-23 21:23:53 +0800

    Add CHDB_VERION for cmake (#96)

--- chdb/vars.sh
+++ chdb/vars.sh
@@ -5,6 +5,9 @@ BUILD_DIR="$PROJ_DIR/buildlib" # build directory
 CHDB_DIR="$PROJ_DIR/chdb" # chdb directory
 CHDB_PY_MOD="_chdb"
 CHDB_PY_MODULE=${CHDB_PY_MOD}$(python3 -c "import sysconfig; print(sysconfig.get_config_var('EXT_SUFFIX'))")
+pushd ${PROJ_DIR}
+CHDB_VERSION=$(python3 -c 'import setup; print(setup.get_latest_git_tag())')
+popd
 
 # check current os type, and make ldd command
 if [ "$(uname)" == "Darwin" ]; then
