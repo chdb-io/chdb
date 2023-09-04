3bfdd2ce61ec0270d70504ceae7b3fd715d22743
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-09-04 12:15:05 +0200

    Create udf.py

---
+++ examples/udf.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from chdb.udf import chdb_udf
+from chdb import query
+
+@chdb_udf()
+def sum_udf(lhs, rhs):
+    return int(lhs) + int(rhs)
+
+print(query("select sum_udf(12,22)"))
