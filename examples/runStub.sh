0d19b9c907f55eee0bfe42d82a168584afcc6a02
Daniel-Robbins <elongaos@gmail.com>
2023-12-19 08:56:53 +0000

    Fix pwd

--- examples/runStub.sh
+++ examples/runStub.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+set -e
+# cd to the directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
 echo "Compile and link"
-clang chdbStub.c -o chdbStub -lc -I../programs/local/ -L../chdb/ -lchdb
+clang chdbStub.c -o chdbStub -I../programs/local/ -L../chdb/ -lchdb
 
 ldd chdbStub
 
