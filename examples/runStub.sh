7ebbccf93eb31ef8e285e35df8adf31c59d47cc8
auxten <auxten@users.noreply.github.com>
2023-12-21 20:27:48 +0800

    Fix runStub.sh for macOS

--- examples/runStub.sh
+++ examples/runStub.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 set -e
+
+# check current os type, and make ldd command
+if [ "$(uname)" == "Darwin" ]; then
+    LDD="otool -L"
+    LIB_PATH="DYLD_LIBRARY_PATH"
+elif [ "$(uname)" == "Linux" ]; then
+    LDD="ldd"
+    LIB_PATH="LD_LIBRARY_PATH"
+else
+    echo "OS not supported"
+    exit 1
+fi
+
 # cd to the directory of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
@@ -8,7 +21,8 @@ cd $DIR
 echo "Compile and link"
 clang chdbStub.c -o chdbStub -I../programs/local/ -L../ -lchdb
 
-LD_LIBRARY_PATH=.. ldd chdbStub
+export ${LIB_PATH}=..
+${LDD} chdbStub
 
 echo "Run it:"
-LD_LIBRARY_PATH=.. ./chdbStub
+./chdbStub
