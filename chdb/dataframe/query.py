63cd0540bf78ef8744241bad373a00fe3d8bae45
auxten <auxtenwpc@gmail.com>
2024-02-05 10:34:27 +0000

    Add show method to fix #78

--- chdb/dataframe/query.py
+++ chdb/dataframe/query.py
@@ -134,6 +134,9 @@ class Table:
         else:
             raise ValueError("Table object is not initialized correctly")
 
+    def show(self):
+        print(self.to_pandas())
+
     def _query_on_path(self, path, sql, **kwargs):
         new_sql = sql.replace("__table__", f'file("{path}", Parquet)')
         res = chdb_query(new_sql, "Parquet", **kwargs)
@@ -235,7 +238,8 @@ class Table:
             if isinstance(tbl, pd.DataFrame):
                 tbl = Table(dataframe=tbl)
             elif not isinstance(tbl, Table):
-                raise ValueError(f"Table {tableName} should be an instance of Table or DataFrame")
+                raise ValueError(
+                    f"Table {tableName} should be an instance of Table or DataFrame")
 
             if tbl._parquet_path is not None:
                 return f'file("{tbl._parquet_path}", Parquet)'
