86ffce81fc946c8d4b8afa87e6fd13e60adb672f
auxten <auxtenwpc@gmail.com>
2024-03-20 13:08:32 +0800

    Save SET statements to set_statements in session path

--- src/Interpreters/InterpreterSetQuery.cpp
+++ src/Interpreters/InterpreterSetQuery.cpp
@@ -8,10 +8,16 @@
 #include <Parsers/ASTQueryWithOutput.h>
 #include <Parsers/ASTSelectWithUnionQuery.h>
 
+#include <fstream>
+
 namespace DB
 {
 
-
+namespace ErrorCodes
+{
+    extern const int CANNOT_OPEN_FILE;
+}
+    
 BlockIO InterpreterSetQuery::execute()
 {
     const auto & ast = query_ptr->as<ASTSetQuery &>();
@@ -20,6 +26,25 @@ BlockIO InterpreterSetQuery::execute()
     session_context->applySettingsChanges(ast.changes);
     session_context->addQueryParameters(ast.query_parameters);
     session_context->resetSettingsToDefaultValue(ast.default_settings);
+
+    // Define the path for the file where SET statements will be logged. Assuming `getContext()->getPath()`
+    // provides a base directory suitable for such logs.
+    auto set_statements_path = fs::path(getContext()->getPath()) / "set_statements";
+    // Open the log file in append mode. If the file doesn't exist, it will be created.
+    std::ofstream set_statements_fs(set_statements_path, std::ofstream::out | std::ofstream::app);
+    if (!set_statements_fs.is_open())
+        throw Exception(ErrorCodes::CANNOT_OPEN_FILE, "Cannot open file {} for appending", set_statements_path.string());
+
+    // Loop through each setting change requested in the SET query. This assumes the primary intent
+    // is to log the names and values of the settings being changed.
+    for (const auto & change : ast.changes)
+    {
+        // Write the SET command for each setting change to the log file, one per line.
+        set_statements_fs << "SET " << change.name << " = " << toString(change.value) << ";\n";
+    }
+
+    // Close the file stream after logging all SET commands.
+    set_statements_fs.close();
     return {};
 }
 
