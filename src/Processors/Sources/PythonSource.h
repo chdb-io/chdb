e1e3d56ce2e74e52a9cc71618eb9569ac9137f0e
auxten <auxtenwpc@gmail.com>
2024-05-08 23:03:18 +0800

    Fix gc for read returned data

--- src/Processors/Sources/PythonSource.h
+++ src/Processors/Sources/PythonSource.h
@@ -1,29 +1,36 @@
 #pragma once
 
-#include <memory>
 #include <Core/Block.h>
 
 #include <Core/ExternalResultDescription.h>
 #include <Processors/ISource.h>
+#include <pybind11/pybind11.h>
 #include <Poco/Logger.h>
 
 namespace DB
 {
 class PyReader;
 
+namespace py = pybind11;
 class PythonSource : public ISource
 {
-    std::shared_ptr<PyReader> reader;
-    Block sample_block;
-    const UInt64 max_block_size;
-
 public:
-    PythonSource(std::shared_ptr<PyReader> reader_, const Block & sample_block_, UInt64 max_block_size_);
+    PythonSource(py::object reader_, const Block & sample_block_, UInt64 max_block_size_);
+    ~PythonSource() override
+    {
+        // Acquire the GIL before destroying the reader object
+        py::gil_scoped_acquire acquire;
+        reader.dec_ref();
+        reader.release();
+    }
 
     String getName() const override { return "Python"; }
     Chunk generate() override;
 
 private:
+    py::object reader;
+    Block sample_block;
+    const UInt64 max_block_size;
     Poco::Logger * logger = &Poco::Logger::get("TableFunctionPython");
     ExternalResultDescription description;
 };
