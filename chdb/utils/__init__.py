647efe4803f1a3769c7ceb37e409cdc2b1c9c7a6
auxten <auxtenwpc@gmail.com>
2024-08-15 19:13:35 +0800

    Fix pylint

--- chdb/utils/__init__.py
+++ chdb/utils/__init__.py
@@ -1,3 +1,8 @@
-from .types import *
+from .types import *  # noqa: F403
 
-__all__ = ["flatten_dict", "convert_to_columnar", "infer_data_type", "infer_data_types"]
+__all__ = [
+    "flatten_dict",
+    "convert_to_columnar",
+    "infer_data_type",
+    "infer_data_types",
+]  # noqa: F405
