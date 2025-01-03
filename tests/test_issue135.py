5d1a7864c326dd6554248743ffbde9b5a66df86c
auxten <auxtenwpc@gmail.com>
2025-01-03 06:04:53 +0000

    Fix UT for refactored Session and chdb.query

--- tests/test_issue135.py
+++ tests/test_issue135.py
@@ -18,30 +18,30 @@ class TestReplaceTable(unittest.TestCase):
         return super().tearDown()
 
     def test_replace_table(self):
-        sess = chs.Session(test_dir)
-        sess.query("CREATE DATABASE IF NOT EXISTS a;", "Debug")
-        sess.query(
-            "CREATE OR REPLACE TABLE a.test (id UInt64, updated_at DateTime DEFAULT now(),updated_at_date Date DEFAULT toDate(updated_at)) "
-            "ENGINE = MergeTree ORDER BY id;"
-        )
-        sess.query("INSERT INTO a.test (id) Values (1);")
-        ret = sess.query("SELECT * FROM a.test;", "CSV")
-        # something like 1,"2023-11-20 21:59:57","2023-11-20"
-        parts = str(ret).split(",")
-        self.assertEqual(len(parts), 3)
-        self.assertEqual(parts[0], "1")
-        # regex for datetime
-        self.assertRegex(parts[1], r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}")
-        # regex for date
-        self.assertRegex(parts[2], r"\d{4}-\d{2}-\d{2}")
+        with chs.Session(test_dir) as sess:
+            sess.query("CREATE DATABASE IF NOT EXISTS a;")
+            sess.query(
+                "CREATE OR REPLACE TABLE a.test (id UInt64, updated_at DateTime DEFAULT now(),updated_at_date Date DEFAULT toDate(updated_at)) "
+                "ENGINE = MergeTree ORDER BY id;"
+            )
+            sess.query("INSERT INTO a.test (id) Values (1);")
+            ret = sess.query("SELECT * FROM a.test;", "CSV")
+            # something like 1,"2023-11-20 21:59:57","2023-11-20"
+            parts = str(ret).split(",")
+            self.assertEqual(len(parts), 3)
+            self.assertEqual(parts[0], "1")
+            # regex for datetime
+            self.assertRegex(parts[1], r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}")
+            # regex for date
+            self.assertRegex(parts[2], r"\d{4}-\d{2}-\d{2}")
 
-        # replace table
-        sess.query(
-            "CREATE OR REPLACE TABLE a.test (id UInt64, updated_at DateTime DEFAULT now(),updated_at_date Date DEFAULT toDate(updated_at)) "
-            "ENGINE = MergeTree ORDER BY id;"
-        )
-        ret = sess.query("SELECT * FROM a.test;", "CSV")
-        self.assertEqual(str(ret), "")
+            # replace table
+            sess.query(
+                "CREATE OR REPLACE TABLE a.test (id UInt64, updated_at DateTime DEFAULT now(),updated_at_date Date DEFAULT toDate(updated_at)) "
+                "ENGINE = MergeTree ORDER BY id;"
+            )
+            ret = sess.query("SELECT * FROM a.test;", "CSV")
+            self.assertEqual(str(ret), "")
 
 
 if __name__ == "__main__":
