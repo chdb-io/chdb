7114d72367f7eab011788e33cfda839b6e9043d9
wudidapaopao <xiaozhe.yu@clickhouse.com>
2025-08-06 20:13:43 +0800

    test: test ci

--- chdb/build/go-example/cgo_darwin.go
+++ chdb/build/go-example/cgo_darwin.go
@@ -5,6 +5,6 @@ package main
 
 /*
 #cgo CFLAGS: -I.
-#cgo LDFLAGS: -mmacosx-version-min=10.15 -L. -Wl,-multiply_defined,suppress -Wl,-force_load,./libchdb.a
+#cgo LDFLAGS: -mmacosx-version-min=10.15 -L. -Wl,-multiply_defined,suppress -Wl,-force_load,./libchdb.a -framework CoreFoundation
 */
 import "C"
