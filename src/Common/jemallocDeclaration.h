b40bc2c9f600ef13935129f0d995afecb19e4905
victor <victorgaoyh@gmail.com>
2025-09-17 17:59:48 +0800

    Update jemalloc header for improved compatibility

--- src/Common/jemallocDeclaration.h
+++ src/Common/jemallocDeclaration.h
@@ -1,12 +1,19 @@
 #pragma once
 
+#ifdef __cplusplus
 #include <cstddef>
+extern "C" {
+#else
+#include <stddef.h>
+#endif
 
 // Forward declarations for jemalloc extended functions
-extern "C" {
 void * mallocx(size_t size, int flags);
 void * rallocx(void * ptr, size_t size, int flags);
 void   sdallocx(void * ptr, size_t size, int flags);
 int    mallctl(const char * name, void * oldp, size_t * oldlenp, void * newp, size_t newlen);
 void   malloc_stats_print(void (*write_cb)(void *, const char *), void * cbopaque, const char * opts);
+
+#ifdef __cplusplus
 }
+#endif
