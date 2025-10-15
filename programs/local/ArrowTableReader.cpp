6d89429c5e51dbe29da1cbe83f3ea59e8995ae76
wudidapaopao <xiaozhe.yu@clickhouse.com>
2025-10-15 14:47:50 +0800

    fix: fix build

--- programs/local/ArrowTableReader.cpp
+++ programs/local/ArrowTableReader.cpp
@@ -183,6 +183,9 @@ Chunk ArrowTableReader::convertArrowArrayToChunk(const ArrowArrayWrapper & arrow
     ArrowColumnToCHColumn converter(
         sample_block,
         "Arrow",
+        format_settings,
+        {},
+        {},
         format_settings.arrow.allow_missing_columns,
         format_settings.null_as_default,
         format_settings.date_time_overflow_behavior,
