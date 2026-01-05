2a57de99266205bb16952187eec4dd2634fd96aa
wudidapaopao <xiaozhe.yu@clickhouse.com>
2026-01-06 02:46:38 +0800

    fix: fix test_parallel_dataframe_query.py

--- programs/local/ChdbClient.cpp
+++ programs/local/ChdbClient.cpp
@@ -58,18 +58,20 @@ ChdbClient::ChdbClient(EmbeddedServerPtr server_ptr)
 
 std::unique_ptr<ChdbClient> ChdbClient::create(EmbeddedServerPtr server_ptr)
 {
-    if (!server_ptr)
-    {
-        server_ptr = EmbeddedServer::getInstance();
-    }
+    chassert(server_ptr);
+
     return std::make_unique<ChdbClient>(server_ptr);
 }
 
 ChdbClient::~ChdbClient()
 {
-    std::lock_guard<std::mutex> lock(client_mutex);
-    cleanup();
-    resetQueryOutputVector();
+    {
+        std::lock_guard<std::mutex> lock(client_mutex);
+        cleanup();
+        resetQueryOutputVector();
+        server.reset();
+    }
+    EmbeddedServer::releaseInstance();
 }
 
 void ChdbClient::cleanup()
@@ -252,7 +254,7 @@ CHDB::QueryResultPtr ChdbClient::executeMaterializedQuery(
 #if USE_PYTHON
         python_table_cache->clear();
 #endif
-        return std::make_unique<CHDB::MaterializedQueryResult>(getExceptionMessage(e, false));
+        return std::make_unique<CHDB::MaterializedQueryResult>(getExceptionMessage(e, true, true));
     }
     catch (...)
     {
